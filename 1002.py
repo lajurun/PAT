edef950509768c373bbb8d00c01b744003a654ff
lajurun <lajunrun@outlook.com>
2024-03-04 14:53:29 +0800

    1001 1002

---
+++ 1002.py
@@ -0,0 +1,15 @@
+n = int(input())
+lst = ['ling', 'yi', 'er', 'san', 'si', 'wu', 'liu', 'qi', 'ba', 'jiu']
+num = 0
+
+while n > 0:
+    num += n % 10
+    n //= 10
+
+s = ''
+while num > 0:
+    i = num % 10
+    num //= 10
+    s = lst[int(i)] + ' ' + s
+
+print(s.strip())
