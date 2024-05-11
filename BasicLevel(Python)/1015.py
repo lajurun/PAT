e04abf28a2c830f019df05ae871d0c2a0ba90a10
lajurun <lajurun@outlook.com>
2024-05-11 10:25:48 +0800

    1015

--- BasicLevel(Python)/1015.py
+++ BasicLevel(Python)/1015.py
@@ -1,8 +1,20 @@
-n, l, h = map(int, input().split())
+# 源代码地址：https://blog.csdn.net/letv0907/article/details/104788854
+# 第一个优化点：input() 改成 sys.stdin.readline()，print() 改成 sys.stdout.write()
+# 第二个优化点：将德分和才分转int，改成从字典里面取值
+# 第三个优化点：优化存储结构
+# 第四个优化点：把代码放到定义的函数里面执行
+# 这里只用到了前面2个优化点，就可以全部通过了
+
+import sys
+
+
+n, l, h = map(int, sys.stdin.readline().split())
 l1, l2, l3, l4 = [], [], [], []
+score_dict = dict(zip([str(i) for i in range(101)], range(101)))
 
 for j in range(n):
-    k, d, c = map(int, input().split())
+    k, d, c = sys.stdin.readline().split()
+    d, c = score_dict[d], score_dict[c]
 
     if d < l or c < l:
         continue
@@ -25,4 +37,5 @@ print(len(l1) + len(l2) + len(l3) + len(l4))
 for i in (l1, l2, l3, l4):
     i.sort(key=st)
     for j in i:
-        print(*j)
+        j = " ".join([str(a) for a in j])
+        sys.stdout.write(j + "\n")
